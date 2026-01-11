f33199f8a81def2a5884e4adf6244143e85410e3
Salvador Cruz <salvador10@ucla.edu>
2026-01-11 10:27:50 -0800

    working on the game

--- Scripts/pipe_pair.gd
+++ Scripts/pipe_pair.gd
@@ -1,15 +1,25 @@
 extends Node2D
 
-var gapHeight = 0
-var gapCenterY = 0
-
+@onready var top_pipe: Area2D = $"Top Pipe"
 @onready var score_line: Area2D = $"Score Line"
+@onready var bottom_pipe: Area2D = $"Bottom Pipe"
 
+var gap_height = randi_range(0,150)
+var half_top_pipe_height = 0
+var half_bottom_pipe_height = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	gapHeight = clamp(randi(), 10, 100)
+	half_top_pipe_height = top_pipe.get_node("CollisionShape2D").shape.size.y /2
+	half_bottom_pipe_height = bottom_pipe.get_node("CollisionShape2D").shape.size.y /2
+	top_pipe.position.y = gap_height/2 + half_top_pipe_height
+	bottom_pipe.position.y = gap_height/2 + half_bottom_pipe_height
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	gap_height = randi_range(25, 500)
+	print(gap_height)
+	top_pipe.position.y = -gap_height/2 - half_top_pipe_height
+	bottom_pipe.position.y = gap_height/2 + half_bottom_pipe_height
+
+	
