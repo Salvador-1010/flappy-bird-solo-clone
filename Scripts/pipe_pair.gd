5ee8ce0ef25090677e34f3c2f6091a3050c3daf1
Salvador Cruz <salvador10@ucla.edu>
2026-01-12 21:14:36 -0800

    Finished up pipe_pair signaling
    sends a signal to increase the points when the player passes it (main game script must do something) and also despwans whenever it exits the screen

--- Scripts/pipe_pair.gd
+++ Scripts/pipe_pair.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var score_line: Area2D = $"Score Line"
 @onready var bottom_pipe: Area2D = $"Bottom Pipe"
 
+
 #creates two separate distance varaibles so that the pipes
 #can have uneeven 
 var top_distance = randi_range(0,150)
@@ -16,7 +17,7 @@ var speed = 200
 var difficulty := 0
 #creates a signal to emit when it spawns
 signal pointScored
-signal pipeOffScreen
+#signal pipeOffScreen
 
 #var to track total pipe gap distance
 var total_gap := 0
@@ -26,6 +27,7 @@ var total_gap := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	score_line.body_entered.connect(_on_score_line_body_entered)
 	top_pipe_height = top_pipe.get_node("CollisionShape2D").shape.size.y
 	bottom_pipe_height = bottom_pipe.get_node("CollisionShape2D").shape.size.y
 	gapRandomizer(difficulty)
@@ -34,11 +36,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	global_position.x += -speed * delta
+	if global_position.x < -335: #335
+		queue_free()
 	
 func setPositions() -> void:
-	top_distance = randi_range(90, 400)
-	bottom_distance = randi_range(90, 340) #340
-	total_gap = top_distance + bottom_distance
 	top_pipe.position.y = -top_distance - top_pipe_height/2
 	bottom_pipe.position.y = bottom_distance + bottom_pipe_height/2
 	
@@ -54,3 +55,8 @@ func gapRandomizer(score: int) -> void:
 		top_distance = randi_range(80, 265)
 		bottom_distance = randi_range(80, 240)
 	
+
+
+func _on_score_line_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Bird_Character"):
+		pointScored.emit()
