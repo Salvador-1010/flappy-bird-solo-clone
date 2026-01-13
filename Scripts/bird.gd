05bba2a0238c56be2c0f272d783d9173389bdd83
Salvador Cruz <salvador10@ucla.edu>
2026-01-12 20:36:16 -0800

    Added movement and randomness to pipe
    Pipe_pair now randomly has a gap on instantiation and moves left at a constant speed

--- Scripts/bird.gd
+++ Scripts/bird.gd
@@ -14,5 +14,5 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 
-
+	
 	move_and_slide()
--- Scripts/pipe_pair.gd
+++ Scripts/pipe_pair.gd
@@ -4,22 +4,53 @@ extends Node2D
 @onready var score_line: Area2D = $"Score Line"
 @onready var bottom_pipe: Area2D = $"Bottom Pipe"
 
-var gap_height = randi_range(0,150)
-var half_top_pipe_height = 0
-var half_bottom_pipe_height = 0
+#creates two separate distance varaibles so that the pipes
+#can have uneeven 
+var top_distance = randi_range(0,150)
+var bottom_distance = randi_range(0, 150)
+var top_pipe_height = 0
+var bottom_pipe_height = 0
+
+var speed = 200
+
+var difficulty := 0
+#creates a signal to emit when it spawns
+signal pointScored
+signal pipeOffScreen
+
+#var to track total pipe gap distance
+var total_gap := 0
+#^^purpose is to ensure that the total distance
+#between the pipes is always at least a specific amount 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	half_top_pipe_height = top_pipe.get_node("CollisionShape2D").shape.size.y /2
-	half_bottom_pipe_height = bottom_pipe.get_node("CollisionShape2D").shape.size.y /2
-	top_pipe.position.y = gap_height/2 + half_top_pipe_height
-	bottom_pipe.position.y = gap_height/2 + half_bottom_pipe_height
-
+	top_pipe_height = top_pipe.get_node("CollisionShape2D").shape.size.y
+	bottom_pipe_height = bottom_pipe.get_node("CollisionShape2D").shape.size.y
+	gapRandomizer(difficulty)
+	setPositions()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	gap_height = randi_range(25, 500)
-	print(gap_height)
-	top_pipe.position.y = -gap_height/2 - half_top_pipe_height
-	bottom_pipe.position.y = gap_height/2 + half_bottom_pipe_height
-
+	global_position.x += -speed * delta
+	
+func setPositions() -> void:
+	top_distance = randi_range(90, 400)
+	bottom_distance = randi_range(90, 340) #340
+	total_gap = top_distance + bottom_distance
+	top_pipe.position.y = -top_distance - top_pipe_height/2
+	bottom_pipe.position.y = bottom_distance + bottom_pipe_height/2
+	
+#generates the gap based on the score (changes difficulty)
+func gapRandomizer(score: int) -> void:
+	if score <= 10:
+		top_distance = randi_range(100, 400)
+		bottom_distance = randi_range(100, 340)
+	elif score > 10 and score <= 25:
+		top_distance = randi_range(90, 325)
+		bottom_distance = randi_range(90, 285)
+	elif score > 25 and score < 50:
+		top_distance = randi_range(80, 265)
+		bottom_distance = randi_range(80, 240)
 	
