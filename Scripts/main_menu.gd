d2b97af95777011a66515132f0e58afd8e6a218c
Salvador Cruz <salvador10@ucla.edu>
2026-02-03 09:42:16 -0800

    still working on settings implementation

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,10 +3,12 @@ extends Control
 var started := false
 signal mouseHovering
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("space") or (event.is_action_pressed("mouse_left")):
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("space") or event.is_action_pressed("mouse_left"):
 		print("event")
 		get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+func _input(event: InputEvent) -> void:
+	print("input")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,7 +18,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_settings_button_mouse_entered() -> void:
-	mouseHovering.emit()
